d1f182a2f11c43d1e6f2a6254dc421873a51bb4d
Escalos <stornblood6969@gmail.com>
2023-02-08 19:56:59 -0500

    Terminado el aumento de horas de los implementos y sus partes(triggers)

--- database/db/triggers/componente_por_implementos/agregar_piezas_por_modelo.sql
+++ database/db/triggers/componente_por_implementos/agregar_piezas_por_modelo.sql
@@ -1,23 +1,20 @@
 DELIMITER //
-CREATE TRIGGER `agregar_piezas_por_modelo` AFTER UPDATE ON `componente_por_implementos` FOR EACH ROW
+CREATE TRIGGER `agregar_piezas_por_modelo` AFTER INSERT ON `componente_por_implementos` FOR EACH ROW
 BEGIN
-    DECLARE pieza INT;
+    DECLARE pieza_id INT;
     DECLARE modelo_del_componente INT;
     DECLARE pieza_final INT DEFAULT 0;
     DECLARE cantidad_de_horas DECIMAL(8,2);
-    DECLARE cursor_piezas CURSOR FOR SELECT articulo_id FROM pieza_por_modelos WHERE articulo_id = new.articulo_id;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET articulo_final = 1;
-    SET cantidad_de_horas = new.horas - old.horas;
+    DECLARE cursor_piezas CURSOR FOR SELECT pieza FROM pieza_por_modelos WHERE articulo_id = new.articulo_id;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET pieza_final = 1;
     OPEN cursor_piezas;
         bucle_piezas:LOOP
             IF pieza_final = 1 THEN
                 leave bucle_piezas;
             END IF;
-                FETCH cursor_piezas INTO pieza;
-                IF NOT EXISTS(SELECT * FROM pieza_por_implementos WHERE pieza = pieza AND articulo_id = new.articulo_id AND estado <> "CAMBIADO") THEN
-                    INSERT INTO pieza_por_implementos (articulo_id,implemento_id,horas,created_at,updated_at) VALUES (pieza,new.id,cantidad_de_horas,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
-                ELSE
-                    UPDATE pieza_por_implementos SET horas = horas + cantidad_de_horas, updated_at = CURRENT_TIMESTAMP() WHERE pieza = pieza AND articulo_id = new.articulo_id AND estado <> "CAMBIADO";
+                FETCH cursor_piezas INTO pieza_id;
+                IF pieza_id is not null THEN
+                    INSERT INTO pieza_por_componentes (pieza,componente_por_implemento_id,horas,created_at,updated_at) VALUES (pieza_id,new.id,new.horas,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP());
                 END IF;
         END LOOP bucle_piezas;
     CLOSE cursor_piezas;

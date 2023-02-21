1f7b56d80fa77d3fd2094cbd2673d598b34cd513
Escalos <stornblood6969@gmail.com>
2023-02-20 22:38:53 -0500

    Terminado la exportacion a excel de reportes de horas

--- app/Exports/TractorReportsExport.php
+++ app/Exports/TractorReportsExport.php
@@ -2,16 +2,55 @@
 
 namespace App\Exports;
 
-use App\Models\ReporteDeTractor;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class TractorReportsExport implements FromCollection
+class TractorReportsExport implements FromCollection,ShouldAutoSize,WithHeadings,WithStyles
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    
+    private $fecha;
+
+    public function __construct($fecha){
+        $this->fecha = $fecha;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Sede',
+            'Fecha',
+            'Turno',
+            'Fundo',
+            'Lote',
+            'Correlativo',
+            'Tractorista',
+            'Tractor',
+            'Horometro Inicial',
+            'Horometro Final',
+            'Implemento',
+            'Labor',
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1    => [
+                        'font' => [
+                                    'bold' => true,
+                                    'size'=> 12
+                                ]
+                    ],
+        ];
+    }
+
     public function collection()
     {
-        return ReporteDeTractor::all();
+        return DB::table('vista_reporte_de_tractores')->select('sede','fecha','turno','fundo','lote','correlativo','name','tractor','horometro_inicial','horometro_final','implemento','labor')->where('sede_id',Auth::user()->sede_id)->where('fecha',$this->fecha)->orderBy('turno')->get();
     }
 }
--- resources/views/livewire/asistente/reporte-de-tractores/botones.blade.php
+++ resources/views/livewire/asistente/reporte-de-tractores/botones.blade.php
@@ -1,6 +1,6 @@
 <div class="grid items-center grid-cols-2 p-6 bg-white sm:grid-cols-4">
     <x-boton-crud accion="$emitTo('asistente.reporte-de-tractores.exportar','abrirModal')" color="gray">Exportar</x-boton-crud>
-    <x-boton-crud accion="abrir_modal(0)" color="green">Registrar</x-boton-crud>
-    <x-boton-crud accion="abrir_modal({{$reporte_id}})" color="amber" :activo="$boton_activo">Editar</x-boton-crud>
+    <x-boton-crud accion="abrirModal(0)" color="green">Registrar</x-boton-crud>
+    <x-boton-crud accion="abrirModal({{$reporte_id}})" color="amber" :activo="$boton_activo">Editar</x-boton-crud>
     <x-boton-crud accion="$emitTo('asistente.reporte-de-tractores.filtros','abrirModal')" color="indigo">Filtros</x-boton-crud>
 </div>

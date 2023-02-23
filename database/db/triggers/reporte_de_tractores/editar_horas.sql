afe74db353d52e4e53cce9803dd759115a2b56dd
Escalos <stornblood6969@gmail.com>
2023-02-23 11:21:24 -0500

    Cambio al poner horometro en reporte

--- database/db/triggers/reporte_de_tractores/editar_horas.sql
+++ database/db/triggers/reporte_de_tractores/editar_horas.sql
@@ -6,6 +6,6 @@ BEGIN
     DECLARE horas DECIMAL(8,2);
     SELECT implemento_id,tractor_id INTO implemento,tractor FROM programacion_de_tractors WHERE id = new.programacion_de_tractor_id LIMIT 1;
     SET horas = new.horometro_final - old.horometro_final;
-	UPDATE tractors SET horometro = horometro + horas WHERE id = tractor;
+	UPDATE tractors SET horometro = new.horometro_final WHERE id = tractor;
     UPDATE implementos SET horas_de_uso = horas_de_uso + horas*(0.85) WHERE id = implemento;
 END $$

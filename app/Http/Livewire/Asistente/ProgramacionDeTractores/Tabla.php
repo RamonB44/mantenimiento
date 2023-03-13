50d2a114bac0e52684086733626e528d18b9d0a1
Escalos <stornblood6969@gmail.com>
2023-03-13 12:54:46 -0500

    Filtro por sede en resumen

--- app/Http/Livewire/Asistente/ProgramacionDeTractores/Tabla.php
+++ app/Http/Livewire/Asistente/ProgramacionDeTractores/Tabla.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Asistente\ProgramacionDeTractores;
 
 use App\Exports\ScheduleSummaryExport;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -30,14 +31,14 @@ class Tabla extends Component
         if($this->fecha == ""){
             $this->emit('alerta',['center','warning','Ingrese la fecha']);
         }else{
-            $resumen_programaciones = DB::table('resumen_de_programacion')->where('fecha',$this->fecha)->select('fundo','labor','numero_de_maquinas','solicitante','turno')->orderBy('solicitante','asc')->orderBy('turno','asc')->get();
-            return Excel::download(new ScheduleSummaryExport($resumen_programaciones,$this->fecha,8),'Resumen de programacion del '.date_format(date_create($this->fecha),'d-m-Y').'.xlsx');
+            $resumen_programaciones = DB::table('resumen_de_programacion')->where('fecha',$this->fecha)->where('sede_id',Auth::user()->sede_id)->select('fundo','labor','numero_de_maquinas','solicitante','turno')->orderBy('solicitante','asc')->orderBy('turno','asc')->get();
+            return Excel::download(new ScheduleSummaryExport($resumen_programaciones),'Resumen de programacion del '.date_format(date_create($this->fecha),'d-m-Y').'.xlsx');
         }
     }
 
     public function render()
     {
-        $resumen_programaciones = DB::table('resumen_de_programacion')->where('fecha',$this->fecha)->orderBy('solicitante','asc')->orderBy('turno','asc')->paginate(6);
+        $resumen_programaciones = DB::table('resumen_de_programacion')->where('fecha',$this->fecha)->where('sede_id',Auth::user()->sede_id)->orderBy('solicitante','asc')->orderBy('turno','asc')->paginate(6);
 
         return view('livewire.asistente.programacion-de-tractores.tabla',compact('resumen_programaciones'));
     }

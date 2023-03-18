e5047117bff33d6889792fde8d8205543b9ba434
Escalos <stornblood6969@gmail.com>
2023-03-18 13:48:55 -0500

    as

---
+++ app/Http/Livewire/Jefe/ProgramacionCultivo/Base.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Jefe\ProgramacionCultivo;
+
+use Livewire\Component;
+
+class Base extends Component
+{
+    public function render()
+    {
+        return view('livewire.jefe.programacion-cultivo.base');
+    }
+}
